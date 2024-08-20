953ebd4383ed44e9a318d317f25c0ff8ce204e41
ykumezawa <umezawa7376789@gmail.com>
2024-08-20 21:58:59 +0900

    commit mainfile

---
+++ weatherproject/weather/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path, include
+from .views import TopView, WeatherAPIView
+
+urlpatterns = [
+    path('top/', TopView.as_view()),
+    path('api/<int:pk>/', WeatherAPIView.as_view())
+
+ 
+]
